cf6a9db2c8a653a1af5fa040232e96a5b715c62c
kmartin <kmartin@ipsosenso.com>
2025-04-17 12:40:43 +0200

    isPrime

--- first-script.js
+++ first-script.js
@@ -1,2 +1,12 @@
-let i = 0;
-console.log(i)
+for (let i = 2; i < 100; i++) {
+	let isPrime = true;
+	for (let j = 2; j < i; j++) {
+		if (i % j === 0) {
+			isPrime = false
+			break
+		}
+	}
+	if (isPrime) {
+		console.log(i)
+	}
+}
