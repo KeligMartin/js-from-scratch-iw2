ef882d5fde5f132dc53b20bdccaa6fc2c98c1d3b
kmartin <kmartin@ipsosenso.com>
2025-05-13 12:02:45 +0200

    dom

--- first-script.js
+++ first-script.js
@@ -1,50 +1,15 @@
-import { Task } from './tasks/task.js';
-import { Owner } from './owners/owner.js';
+const title = document.getElementById("title")
+title.innerText = "j'ai été modifié "
 
-const task1 = new Task("Tache 1", "description 1", 1, "Kelig")
-const task2 = new Task("Tache 2", "description 2", 10)
-const task3 = new Task("Tache 3", "description 3", 7)
-const task4 = new Task("Tache 4", "description 4", 57)
-const task5 = new Task("Tache 5", "description 5", 0)
+// <div> blabla </div> blabla => innerText
 
-const tasks = [task1, task2, task3, task4, task5]
+const button = document.getElementById('btn-1')
+button.addEventListener('click', () => console.log('test'))
 
-const tasksUrgentes = tasks.filter((element) => element.priority < 5)
-
-// filter natif
-const tasksUrgentesNatives = []
-for(let i = 0; i < tasks.length; i++) {
-	const element = tasks[i]
-	if(element.priority < 5) {
-		tasksUrgentesNatives.push(element)
-	}
+function write()  {
+	console.log('tu es en train d ecrire')
 }
 
-function doublePriority(priority) {
-	return priority * 2
-}
-
-const tasksDoublees = tasks.map((element) => doublePriority(element.priority))
-// natif
-const tasksDoubleesNatif = []
-for(let i = 0; i < tasks.length; i++) {
-	const element = tasks[i]
-	element.priority = element.priority * 2
-	tasksDoubleesNatif.push(element)
-}
-
-const totalPriority = tasks.reduce((acc, currentValue) => acc + currentValue.priority, 0)
-
-// natif
-let total = 0
-for(let i = 0; i < tasksDoublees.length; i++) {
-	total = total + tasksDoublees[i]
-}
-
-console.table(tasks.sort((a, b) => a.priority - b.priority))
-console.table(tasksUrgentes)
-console.table(tasksUrgentesNatives)
-console.table(tasksDoubleesNatif)
-console.log(totalPriority)
-
-// fuction sort()
+const input = document.getElementById('input-text')
+input.addEventListener('focus', write)
+input.addEventListener('blur', () => console.log('tu as fini d ecrire'))
