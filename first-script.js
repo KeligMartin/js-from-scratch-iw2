9c7ccb75aee6b53207083731b72c721df5e9b53c
kmartin <kmartin@ipsosenso.com>
2025-05-13 09:43:57 +0200

    first script

--- first-script.js
+++ first-script.js
@@ -1,12 +1,19 @@
-for (let i = 2; i < 100; i++) {
-	let isPrime = true;
-	for (let j = 2; j < i; j++) {
-		if (i % j === 0) {
-			isPrime = false
-			break
-		}
-	}
-	if (isPrime) {
-		console.log(i)
+class Task {
+	constructor(title, description, priority) {
+		this.title = title
+		this.description = description
+		this.priority = priority
 	}
 }
+
+const task1 = new Task("Tache 1", "description 1", 1)
+const task2 = new Task("Tache 2", "description 2", 10)
+const task3 = new Task("Tache 3", "description 3", 7)
+const task4 = new Task("Tache 4", "description 4", 57)
+const task5 = new Task("Tache 5", "description 5", 0)
+
+const tasks = [task1, task2, task3, task4, task5]
+
+console.table(tasks.sort((a, b) => a.priority - b.priority))
+
+// fuction sort()
