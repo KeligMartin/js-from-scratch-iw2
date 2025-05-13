dc7b4dce98b9baff71126d5e31459f95dc263b43
kmartin <kmartin@ipsosenso.com>
2025-05-13 13:03:46 +0200

    manipulation du dom

--- first-script.js
+++ first-script.js
@@ -1,15 +1,60 @@
-const title = document.getElementById("title")
-title.innerText = "j'ai été modifié "
+import { Task } from './tasks/task.js';
 
-// <div> blabla </div> blabla => innerText
+const table = document.getElementById('content')
+const task1 = new Task('Task 1', 'créer le site web', 0)
+const task2 = new Task('Task 2', 'importer bootstrap', 10)
+const task3 = new Task('Task 3', 'créer le script', 1)
+const task4 = new Task('Task 4', 'créer les tasks', 2)
+const task5 = new Task('Task 5', 'ajouter les tasks au tableau', 3)
 
-const button = document.getElementById('btn-1')
-button.addEventListener('click', () => console.log('test'))
+const tasks = [task1, task2, task3, task4, task5]
 
-function write()  {
-	console.log('tu es en train d ecrire')
+const priority = document.getElementById('priority')
+
+tasks.forEach((task) => {
+	createTaskNode(task)
+})
+
+const tablesRows = document.getElementsByClassName('taskRow')
+priority.addEventListener('click', () => {
+	while (tablesRows.length > 0) {
+		tablesRows[0].parentNode.removeChild(tablesRows[0])
+	}
+	const sortedTasks = tasks.sort((a, b) => a.priority - b.priority)
+	sortedTasks.forEach((task) => {
+		createTaskNode(task)
+	})
+})
+
+const addTask = document.getElementById('add-task')
+
+addTask.addEventListener('click', () => {
+	const task = new Task('Nouvelle Task', 'nouvelle description', 10)
+	tasks.push(task)
+	createTaskNode(task)
+})
+
+
+function createTaskNode(task) {
+	const taskElement = document.createElement('tr')
+	// <tr></tr>
+	taskElement.classList.add('taskRow')
+	initCellTable(taskElement, task.title)
+	initCellTable(taskElement, task.description)
+	initCellTable(taskElement, task.priority)
+	// <tr><td>titre</td><td>desc
+	table.appendChild(taskElement)
 }
 
-const input = document.getElementById('input-text')
-input.addEventListener('focus', write)
-input.addEventListener('blur', () => console.log('tu as fini d ecrire'))
+function initCellTable(taskElement, taskValue) {
+	const taskCell = document.createElement('td')
+	taskCell.innerText = taskValue
+	taskElement.appendChild(taskCell)
+}
+
+const alert = () => {
+	if (tablesRows.length >= 5) {
+		window.alert('trop de lignes !')
+	}
+}
+alert()
--- tasks/task.js
+++ tasks/task.js
@@ -1,15 +1,9 @@
 class Task {
-	#author
 
 	constructor(title, description, priority, author) {
 		this.title = title
 		this.description = description
 		this.priority = priority
-		this.#author = author
-	}
-
-	get author() {
-		return this.author
 	}
 }
 
